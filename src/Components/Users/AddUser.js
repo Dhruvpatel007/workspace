4d3cd6787f7ca30af771ec826bbbb8f8e1d483af
Dhruv patel <69782004+Dhruvpatel007@users.noreply.github.com>
2023-06-01 10:09:21 +0000

    project completed

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -38,9 +38,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () =>{
+    setError(null);
+  }
+
   return (
     <div>
-    {error && <ErrorModel title={error.title} message={error.message}/>}
+    {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorHandler}/>}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
