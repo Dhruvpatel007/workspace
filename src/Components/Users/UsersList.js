0d91fd80d17c5ede5f9275221928160024970dc5
Dhruv patel <69782004+Dhruvpatel007@users.noreply.github.com>
2023-06-01 10:20:56 +0000

    "Initial commit"

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -4,21 +4,9 @@ import classes from './UsersList.module.css'
 const UsersList = (props) => {
     return (
       <Card className={classes.users}>
-<<<<<<< HEAD
         <ul >
           {props.users.map((user, index) => (
             <li key={index}>
-=======
-<<<<<<< HEAD
-        <ul >
-          {props.users.map((user, index) => (
-            <li key={index}>
-=======
-        <ul>
-          {props.users.map((user) => (
-            <li>
->>>>>>> e70f24f2b11d80128c738baf3bdba533dc7f48bd
->>>>>>> 57a73fc7eb36e44cab52aa82a8f21568ab6836d5
               {user.name} ({user.age} year old)
             </li>
           ))}
